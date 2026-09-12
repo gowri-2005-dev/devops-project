24be98bde0b22855dd54abe7efd3ba9d718baa97
gowri-2005-dev <gowrikowsalya2005@gmail.com>
2026-09-12 20:04:18 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 app.get("/", (req,res) => {
-    res.send("Hello! My devops project is running");
+    res.send("Hello! My devops project is running on AWS EC2");
 });
 app.listen(PORT,() => {
     console.log(`server is runnning on port ${PORT}`);
